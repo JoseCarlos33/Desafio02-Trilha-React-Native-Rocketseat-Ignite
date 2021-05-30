5eadc5e640b11bf28ffa5cc8c03da3cfa181edb7
JoseCarlos33 <joseph.finder33@gmail.com>
2021-05-30 15:23:19 -0300

    DESAFIO 02 - IGNITE

---
+++ README.md
@@ -0,0 +1,21 @@
+<h1 align="center">
+  DEMONSTRAÇÃO
+</h1>
+
+<p align="center">
+  <img src="./src/assets/demoDesafio02.gif" alt="demo-mobile" height="425">
+</p>
+
+## Sobre o projeto: 
+**Este desafio é uma continuação do [Desafio 01](https://github.com/JoseCarlos33/Desafio01-Trilha-React-Native-Rocketseat-Ignite) do curso Ignite da Rocketseat, que consiste da criação de um botão que possibilite a troca de temas após ser clicado**
+
+## Para este projeto foi utilizado:
+
+* [ ] React Native
+* [ ] TypeScript
+* [ ] Context API
+* [ ] Icon
+
+## Autor
+
+**José Carlos Noronha**
---
+++ src/components/ThemeSwitch.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import { Text, View, Switch, StyleSheet, TouchableOpacity } from "react-native";
+import {useColorTheme} from '../Contexts/ColorTheme';
+
+import Icon from 'react-native-vector-icons/Feather'
+
+export function ThemeSwitch(){
+  const [isEnabled, setIsEnabled] = useState(false);
+  const {nightMode, setNightMode} = useColorTheme();
+  
+  
+  const toggleSwitch = () => {
+    setIsEnabled(previousState => !previousState); 
+    setNightMode(!nightMode)
+  }
+
+  return (
+    <View>
+      <TouchableOpacity 
+        onPress={toggleSwitch} 
+        style={[styles.container, {backgroundColor: nightMode
+                                                  ? '#988BC7'
+                                                  : '#3FAD27'}]}
+      >
+        <Icon
+          name={nightMode ? 'moon' : 'sun'}
+          size={20}
+          color= "#FFFF"
+          style={{fontWeight: 'bold'}}
+        />
+      </TouchableOpacity>
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    position: 'absolute',
+    borderRadius: 15,
+    width: 30,
+    height: 30,
+    alignItems:'center',
+    justifyContent: 'center',
+    // elevation: 5
+  },
+
+});
